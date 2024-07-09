dd80c50d966748b56a74f5572746dbdefa360091
Victor Ikechukwu Nwafor <dixonsilverofficial@gmail.com>
2024-07-09 07:26:36 +0100

    Added Slider to Landing Page

---
+++ src/data.js
@@ -0,0 +1,108 @@
+const Listings = [
+   {
+      id: 1,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/Frame 26.jpg',
+      colornum: '1 Colour'
+   },
+   {
+      id: 2,
+      name: 'Novaweave Phantom Pulse',
+      gender: 'Men',
+      price: '100,540',
+      image: './public/product-images-optimize/Frame 27.jpg',
+      colornum: '3 Colours'
+   },
+   {
+      id: 3,
+      name: 'Atlas Ascent GTX',
+      gender: 'Women',
+      price: '260,000',
+      image: './public/product-images-optimize/Frame 28.jpg',
+      colornum: '3 Colours'
+   },
+   {
+      id: 4,
+      name: 'Element MTG 1000',
+      gender: 'Women',
+      price: '500,000',
+      image: './public/product-images-optimize/Frame 29.jpg',
+      colornum: '1 Colour'
+   },
+   {
+      id: 5,
+      name: 'Novaweave Phantom Pulse',
+      gender: 'Women',
+      price: '100,540',
+      image: './public/product-images-optimize/Frame 31.jpg',
+      colornum: '3 Colours'
+   },
+   {
+      id: 6,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/Frame 33.jpg',
+      colornum: '3 Colours'
+   },
+   {
+      id: 7,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/Frame 35.jpg',
+      colornum: '1 Colour'
+   },
+   {
+      id: 8,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/Frame 37.jpg',
+      colornum: '1 Colour'
+   },
+   {
+      id: 9,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/image 11.jpg',
+      colornum: '3 Colours'
+   },
+   {
+      id: 10,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/image 15.jpg',
+      colornum: '3 Colours'
+   },
+   {
+      id: 11,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/image 18.jpg',
+      colornum: '3 Colours'
+   },
+   {
+      id: 12,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/image 21.jpg',
+      colornum: '3 Colours'
+   },
+   {
+      id: 13,
+      name: 'Zephyr Swiftstrike Pro',
+      gender: 'Men',
+      price: '140,540',
+      image: './public/product-images-optimize/image 29.jpg',
+      colornum: '3 Colours'
+   }
+];
+
+export default Listings;
